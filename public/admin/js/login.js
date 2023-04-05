7421efb8d9dda476ed585799a9d0865bf3b2abb1
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-04-05 04:55:13 +0100

    JS

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,5 +1,5 @@
-// var BASE_URL = "https://concise.cfcing.org/";
-var BASE_URL = "http://127.0.0.1:8000/";
+var BASE_URL = "https://concise.cfcing.org/";
+// var BASE_URL = "http://127.0.0.1:8000/";
 var ADMIN_URL = BASE_URL + "dashboard/";
 if (localStorage.getItem('token') == null || localStorage.getItem('token') == '') {
     window.location = ADMIN_URL + 'login'
